9be1cd0635f7a82c9b5ac725cf21e81855f314d4
chushoga <chushoga@gmail.com>
2018-03-27 18:44:21 +0900

    update

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -6,11 +6,59 @@ using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour {
 
+	// --------------------------------
+	// GAME OVER
+	// --------------------------------
 	public GameObject gameOverScreen;
 	private CanvasGroup gameOverPanel; 
 
+	// --------------------------------
+	// FADE SCREEN
+	// --------------------------------
+	private float fadeSpeed = 1f;
+	private Canvas fadeCanvas;
+	private Transform coverImageGO; // black overlay
+	private Image coverImage; // child of the black overlay
+	// --------------------------------
+
+
 	void Start(){
-		
+
+		// FADE SCREEN SETUP
+		fadeCanvas = GetComponentInChildren<Canvas>();
+		coverImageGO = fadeCanvas.transform.Find("Image");
+		coverImage = coverImageGO.GetComponentInChildren<Image>();
+
+		// start by enabeling the image
+		// the default is false -> hidden
+		coverImage.enabled = true;
+
+		// start with fading in.
+		CrossAlphaWithCallback(coverImage, 0f, fadeSpeed, delegate {
+			coverImage.enabled = false;
+		});
+
+	}
+
+	public void CrossAlphaWithCallback(Image img, float alpha, float duration, System.Action action){
+		StartCoroutine(CrossFadeAlphaCOR(img, alpha, duration, action));
+	}
+
+	IEnumerator CrossFadeAlphaCOR(Image img, float alpha, float duration, System.Action action){
+		img.enabled = true;
+
+		Color currentColor = img.color;
+		Color visibleColor = img.color;
+		visibleColor.a = alpha;
+
+		float counter = 0;
+
+		while(counter < duration){
+			counter += Time.deltaTime;
+			img.color = Color.Lerp(currentColor, visibleColor, counter / duration);
+			yield return null;
+		}
+		action.Invoke();
 	}
 
 	// Show the game over screen
---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	// Set the target frame rate
+	void Awake(){
+		Application.targetFrameRate = 60;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
