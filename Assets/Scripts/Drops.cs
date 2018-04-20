4965193126c5d8cf77d650b73f1b645f4d67dd5f
chushoga <chushoga@gmail.com>
2018-04-20 14:33:30 +0900

    added a drop spawner
    TODO: make random rotation and spawn position in the radius

---
+++ Assets/Scripts/Drops.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drops : MonoBehaviour {
+
+	public float health;
+	public int points; // points for destroying/hitting the target
+	public int plusTime = 0; // extra time to add if hit.
+	private bool hasCounted; // has the point been counted yet?
+	public float rarity; // rarity of the target.
+	public float speed; // movement speed
+
+	private bool isMoving = true;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.useGravity = false;
+		gameObject.AddComponent<ConstantForce>().force = new Vector3(0.0f, -0.2f, 0.0f); // add a constant downward force
+		rb.AddForce(Vector3.down, ForceMode.Force);
+	}
+	
+	void FixedUpdate(){
+		if(isMoving) {
+				//transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+	}
+
+	void OnTriggerEnter(Collider collider){
+
+		if (collider.gameObject.tag == "OutOfBounds_Platform"){
+			// un parent it with the platform so it does not keep following the patform when hit.
+			// gameObject.transform.SetParent(null); // is this needed now??
+			isMoving = false;
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+
+		gameObject.GetComponent<Rigidbody>().useGravity = true; // enable gravity
+		//gameObject.transform.SetParent(null); // clear parent? is this needed now?
+
+		// IF COLLISION IS BULLET
+		if(collision.gameObject.tag == "Ammo") {
+			GameManager.SCORE += points;
+			GameManager.TIME_REMAINING += plusTime;
+		}
+
+		// IF COLLISION IS TARGET
+		if(collision.gameObject.tag == "Target"){
+			GameManager.SCORE += collision.gameObject.GetComponent<Drops>().points;
+			Destroy(gameObject, 0.5f);
+		}
+
+	}	
+}
---
+++ Assets/Scripts/Managers/DropManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropManager : MonoBehaviour {
+
+	public bool isSpawning = true;
+	public float dropRadius = 10.0f;
+	public float spawnRate = 1.0f;
+	public List<GameObject> itemsToDrop = new List<GameObject>();
+
+	public GameObject spawnPoint;
+
+	private GameObject dropContainer; // a container for the spawned targets. This is just for keeping editor clean.
+
+	// Use this for initialization
+	void Start () {
+		dropContainer = new GameObject();
+		dropContainer.name = "DROP_CONTAINER";
+
+		StartCoroutine(SpawnDropsTimer()); // start spawn timer
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// ---------------------------------------------------------------------------------------------------------------------------------------------
+	// AIR TARGETS
+	// ---------------------------------------------------------------------------------------------------------------------------------------------
+	void SpawnDrops(){
+
+		Vector3 randomX = new Vector3(spawnPoint.transform.position.x + Random.Range(-1.0f, 1), spawnPoint.transform.position.y + Random.Range(-1.0f, 1), spawnPoint.transform.position.z + Random.Range(-1.0f, 1));
+
+		GameObject go = Instantiate(itemsToDrop[ChooseRandomSpawn(itemsToDrop)], randomX, Quaternion.identity);
+		go.transform.parent = dropContainer.transform;
+
+	}
+
+	public IEnumerator SpawnDropsTimer(){
+		while(isSpawning) {
+			yield return new WaitForSeconds(spawnRate);
+			SpawnDrops();
+		}
+	}
+
+
+	// ---------------------------------------------------------------------------------------------------------------------------------------------
+	// OTHER FUNCTIONS
+	// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+	// Return an index for a random object to spawn from a list of prefabs.
+	// This will return and index depending on the prefabs rarity weight.
+	// Keep rarity between 0.001 - 100 on prefab stats.
+	public int ChooseRandomSpawn(List<GameObject> list){
+
+		float x = 0; // counter
+		float totalRarity = 0; // tht total rarity weight of all in the list
+		int index = 0; // return this index
+
+		if(list.Count >= 0) {
+
+			// get total rarity
+			for(int i = 0; i < list.Count; i++) {
+
+				// check if target or obsticle
+				if(list[i].GetComponentsInChildren<Drops>().Length != 0) {
+					Drops tmScript = list[i].GetComponentInChildren<Drops>();
+					totalRarity += tmScript.rarity;
+				} else {
+					totalRarity += 0;
+				}
+			}
+
+			// get random number from the total rarity weight
+			x = Random.Range(0, totalRarity);
+
+			// Step through the list and check if x is less than the rarity.
+			// If x is less than the rarity then break out of the loop and 
+			// return the index;
+			for(int i = 0; i < list.Count; i++) {
+
+				index = i;
+
+				//TargetManager tmScript = list[i].GetComponentInChildren<TargetManager>();
+				//float rarity = tmScript.rarity;
+				float rarity = 0;
+
+				// check if target or obsticle
+				if(list[i].GetComponentsInChildren<Drops>().Length != 0) {
+					Drops tmScript = list[i].GetComponentInChildren<Drops>();
+					rarity = tmScript.rarity;
+				} else {	
+					rarity = 0;
+				}
+
+
+				if(x <= rarity) {
+					break;
+				}
+
+				x -= rarity;
+
+			}
+		}
+		return index;
+	}
+}
