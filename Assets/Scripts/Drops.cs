b15364eb26c7eee4ad777a50611406fbbae124f2
chushoga <chushoga@gmail.com>
2018-04-25 14:12:53 +0900

    added some drops code

--- Assets/Scripts/Drops.cs
+++ Assets/Scripts/Drops.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Drops : MonoBehaviour {
 
-	public float health;
+	public int hp = 0;
+	private int maxHp;
 	public int points; // points for destroying/hitting the target
 	public int plusTime = 0; // extra time to add if hit.
 	private bool hasCounted; // has the point been counted yet?
@@ -14,12 +15,22 @@ public class Drops : MonoBehaviour {
 	private bool isMoving = true;
 	private Rigidbody rb;
 
+	// for changing HP color
+	private Renderer rend;
+	private Color red = Color.red;
+	private Color yellow = Color.yellow;
+	private Color grey = Color.grey;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 		rb.useGravity = false;
 		gameObject.AddComponent<ConstantForce>().force = new Vector3(0.0f, -0.2f, 0.0f); // add a constant downward force
 		rb.AddForce(Vector3.down, ForceMode.Force);
+
+		rend = gameObject.GetComponent<Renderer>();
+
+		maxHp = hp;
 	}
 	
 	void FixedUpdate(){
@@ -38,21 +49,44 @@ public class Drops : MonoBehaviour {
 	}
 
 	void OnCollisionEnter(Collision collision){
-
-		gameObject.GetComponent<Rigidbody>().useGravity = true; // enable gravity
-		//gameObject.transform.SetParent(null); // clear parent? is this needed now?
+		
+		if(collision.collider.GetType() == typeof(SphereCollider)) {
+			gameObject.GetComponent<Rigidbody>().useGravity = true; // enable gravity
+			rb.AddForce(Vector3.zero, ForceMode.Force);
+			Destroy(transform.GetChild(0).gameObject);
+		}
 
 		// IF COLLISION IS BULLET
 		if(collision.gameObject.tag == "Ammo") {
-			GameManager.SCORE += points;
-			GameManager.TIME_REMAINING += plusTime;
+			hp -= collision.gameObject.GetComponent<AmmoManager>().damage;
+			if(hp <= 0) {
+				GameManager.SCORE += points;
+				GameManager.TIME_REMAINING += plusTime;
+				Destroy(gameObject, 0.5f);
+			}
 		}
 
 		// IF COLLISION IS TARGET
-		if(collision.gameObject.tag == "Target"){
-			GameManager.SCORE += collision.gameObject.GetComponent<Drops>().points;
-			Destroy(gameObject, 0.5f);
+		// remove half of the targets HP
+		if(collision.gameObject.tag == "Target") {
+			hp -= hp / 2;
+			if(hp <= 0) {
+				GameManager.SCORE += collision.gameObject.GetComponent<StationaryTarget>().points;
+				Destroy(gameObject, 0.5f);
+			}
 		}
 
-	}	
+		// TEMPERARY DAMAGE INDICATOR
+		if(hp >= 1 && hp <= (maxHp / 4)) {
+			// less than 25% red
+			rend.material.color = red;
+		} else if (hp > (maxHp / 4) && hp <= (maxHp / 2)) {
+			// if less than 50% yellow
+			rend.material.color = yellow;
+		} else if (hp >= (maxHp / 2) && hp <= (maxHp - 1)) {
+			// if less than 100% grey
+			rend.material.color = grey;
+		}
+	}
+
 }
