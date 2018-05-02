40efcd9f190ccbea3e981df9c274194ab45651ed
chushoga <chushoga@gmail.com>
2018-05-03 02:08:00 +0900

    update
    update

--- Assets/Scripts/Prototyping/JumpController.cs
+++ Assets/Scripts/Prototyping/JumpController.cs
@@ -8,9 +8,9 @@ public class JumpController : MonoBehaviour
 	private Vector3 LAUNCH_VELOCITY = new Vector3(20f, 80f, 0f);
 	private Vector3 INITIAL_POSITION = Vector3.zero;
 	private readonly Vector3 GRAVITY = new Vector3(0f, -240.0f, 0f);
-	private const float DELAY_UNTIL_LAUNCH = 20f;
+	private const float DELAY_UNTIL_LAUNCH = 5f;
 	private int NUM_DOTS_TO_SHOW = 10;
-	private float DOT_TIME_STEP = 0.025f; // Balance this
+	private float DOT_TIME_STEP = 0.05f; // Balance this
 
 	private bool launched = false;
 	private float timeUntilLaunch = DELAY_UNTIL_LAUNCH;
