d3b887720faae8a0eb0e172debf5b550d8f04a90
chushoga <chushoga@gmail.com>
2018-05-02 16:26:17 +0900

    Fixing trajectory bug
    fixed? still a small discrepency, try to find

--- Assets/Scripts/Prototyping/TestTurretController.cs
+++ Assets/Scripts/Prototyping/TestTurretController.cs
@@ -8,7 +8,7 @@ public class TestTurretController : MonoBehaviour {
 	GameObject barrel;
 	public Transform ammoSpawn;
 	public GameObject ammo;
-	public float shotForce = 80.0f;	
+	public float shotForce = 50.0f;	
 	public Text powerTxt;
 
 
@@ -16,12 +16,10 @@ public class TestTurretController : MonoBehaviour {
 	private Vector3 INITIAL_POSITION = Vector3.zero;
 	private readonly Vector3 GRAVITY = new Vector3(0f, -240.0f, 0f);
 	private const float DELAY_UNTIL_LAUNCH = 20f;
-	private int NUM_DOTS_TO_SHOW = 15;
-	private float DOT_TIME_STEP = 0.025f; // Balance this
+	private int NUM_DOTS_TO_SHOW = 10;
+	private float DOT_TIME_STEP = 0.05f; // Balance this
 
 	private bool launched = false;
-	private float timeUntilLaunch = DELAY_UNTIL_LAUNCH;
-	private Rigidbody rigidBody;
 
 	public GameObject trajectoryDotPrefab;
 	private MeshRenderer mr;
@@ -34,31 +32,35 @@ public class TestTurretController : MonoBehaviour {
 	void Start () {
 		barrel = transform.Find("Body/Barrel").gameObject;
 		Physics.gravity = new Vector3(0, -240.0f, 0);
-		rigidBody = GetComponent<Rigidbody>();
 		trajectoryMaster = new GameObject();
 		trajectoryMaster.name = "trajectoryMaster";
+
+		INITIAL_POSITION = ammoSpawn.transform.position;
+		LAUNCH_VELOCITY_NEW = ammoSpawn.transform.forward * shotForce;
+		DrawTrajectory();
 	}
 
-	void Update () {
+	void FixedUpdate () {
+		INITIAL_POSITION = ammoSpawn.transform.position;
+		LAUNCH_VELOCITY_NEW = ammoSpawn.transform.forward * shotForce;
 		
 		//if(Input.GetKey(KeyCode.A)){ transform.Rotate(Vector3.down, Time.deltaTime * 50.0f); }
 
 		//if(Input.GetKey(KeyCode.D)){ transform.Rotate(Vector3.up, Time.deltaTime * 50.0f); }
 
-		if(Input.GetKey(KeyCode.W)){ barrel.transform.Rotate(Vector3.forward, Time.deltaTime * 50.0f); }
+		if(Input.GetKey(KeyCode.W)){ barrel.transform.Rotate(Vector3.forward, Time.deltaTime * 50.0f); DrawTrajectory(); }
 
-		if(Input.GetKey(KeyCode.S)){ barrel.transform.Rotate(Vector3.back, Time.deltaTime * 50.0f); }
+		if(Input.GetKey(KeyCode.S)){ barrel.transform.Rotate(Vector3.back, Time.deltaTime * 50.0f); DrawTrajectory(); }
 
-		if(Input.GetMouseButtonDown(0)){ Shoot(); }
+		//if(Input.GetMouseButtonDown(0)){ Shoot(); }
 
 		powerTxt.text = shotForce + "p";
 	}
 
 	public void Shoot(){
 		GameObject bullet = Instantiate(ammo, ammoSpawn.transform.position, ammoSpawn.transform.rotation);
-		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * (shotForce);
-		Debug.Log(bullet.transform.forward * (shotForce) + "<--------");
-		//Debug.Log(bullet.GetComponent<Rigidbody>().velocity);
+		LAUNCH_VELOCITY_NEW = ammoSpawn.transform.forward * shotForce;
+		bullet.GetComponent<Rigidbody>().velocity = LAUNCH_VELOCITY_NEW;
 	}
 
 	public void IncreasePower(){
@@ -72,31 +74,32 @@ public class TestTurretController : MonoBehaviour {
 	}
 
 	public void DrawTrajectory(){
-		Debug.Log("child count = " + trajectoryMaster.transform.childCount);
+
+		LAUNCH_VELOCITY_NEW = ammoSpawn.transform.forward * shotForce;
+		INITIAL_POSITION = ammoSpawn.transform.position;
 
 		if(trajectoryMaster.transform.childCount == 0) {
-			for (int i = 0; i < NUM_DOTS_TO_SHOW; i++) {			
-					float p;
-					p = 1 - ((float)i / NUM_DOTS_TO_SHOW);
-					//Debug.Log(i + " / " + NUM_DOTS_TO_SHOW + " = " + p);
-					GameObject trajectoryDot = Instantiate(trajectoryDotPrefab);
-					trajectoryDot.transform.parent = trajectoryMaster.transform;
-					Material col = trajectoryDot.GetComponent<Renderer>().material;
-					trajectoryDot.GetComponent<Renderer>().material.color = new Color(col.color.r,col.color.g, col.color.b, p);
-					//trajectoryDot.GetComponent<Renderer>().material.color = Color.green;
-					trajectoryDot.transform.position = CalculatePosition(DOT_TIME_STEP * i);
+			for (int i = 0; i < NUM_DOTS_TO_SHOW; i++) {
+					
+				float p;
+				p = 1 - ((float)i / NUM_DOTS_TO_SHOW);
+
+				GameObject trajectoryDot = Instantiate(trajectoryDotPrefab);			
+				trajectoryDot.transform.parent = trajectoryMaster.transform;
+
+				Material col = trajectoryDot.GetComponent<Renderer>().material;
+				trajectoryDot.GetComponent<Renderer>().material.color = new Color(col.color.r,col.color.g, col.color.b, p);		
+				trajectoryDot.transform.position = CalculatePosition(DOT_TIME_STEP * i);
 			}
 
 		} else {
 			// transform the position of the dots here!!!
 			for (int i = 0; i < trajectoryMaster.transform.childCount; i++) {			
+
 				float p;
 				p = 1 - ((float)i / NUM_DOTS_TO_SHOW);
 
 				GameObject trajectoryDot = trajectoryMaster.transform.GetChild(i).gameObject;
-
-				Material col = trajectoryDot.GetComponent<Renderer>().material;
-				trajectoryDot.GetComponent<Renderer>().material.color = new Color(col.color.r,col.color.g, col.color.b, p);
 				trajectoryDot.transform.position = CalculatePosition(DOT_TIME_STEP * i);
 			}
 		}
@@ -105,27 +108,9 @@ public class TestTurretController : MonoBehaviour {
 	private Vector2 CalculatePosition(float elapsedTime)
 	{
 		//return GRAVITY * elapsedTime * elapsedTime * 0.5f + LAUNCH_VELOCITY * elapsedTime + INITIAL_POSITION;
-		//Debug.Log(barrel.transform.rotation * (shotForce) + "<--");
 
-		float angleInDegrees;
-		Vector3 rotationAxis;
-		barrel.transform.rotation.ToAngleAxis(out angleInDegrees, out rotationAxis);
-
-		Vector3 angularDisplacement = rotationAxis * angleInDegrees * Mathf.Deg2Rad;    
-		Vector3 angularSpeed = angularDisplacement / Time.deltaTime;
-
-		//Debug.Log(angularSpeed + " *-------*");
-
-		LAUNCH_VELOCITY_NEW = ammoSpawn.transform.forward * (shotForce);
-		Debug.Log(LAUNCH_VELOCITY_NEW + " *-------*");
-
-		return GRAVITY * elapsedTime * elapsedTime * 0.5f + LAUNCH_VELOCITY_NEW * elapsedTime + barrel.transform.position;
+		return GRAVITY * elapsedTime * elapsedTime * 0.5f + LAUNCH_VELOCITY_NEW * elapsedTime + INITIAL_POSITION;
 	}
 		
-	private void Launch()
-	{
-		rigidBody.velocity = LAUNCH_VELOCITY;
 
-		launched = true;
-	}
 }
